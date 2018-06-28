3376f0a841d6fc9b1d6321361b95f418e12613e4
tsound922 <oushirx@gmail.com>
2018-06-29 02:01:12 +1000

    Practice with Store, Reducers and Provider

---
+++ dev/js/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//require('../../scss/style.scss');
+const App = () => (
+    <div>
+        <h2>
+            Username List:
+            <hr/>
+        </h2>
+        <h2>
+            User Details:
+        </h2>
+    </div>
+);
+
+export default App;
